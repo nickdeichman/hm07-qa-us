6ca8a5ce45c66e21cbaebf43b88eb3cd51a8f3ef
nickdeichman <nickdeichman@gmail.com>
2024-10-12 13:14:45 +0300

    update server link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   API_URL:
-    'https://cnt-513af85e-f097-4447-97c8-67fc26cf478a.containerhub.tripleten-services.com',
+    'https://cnt-30f5e659-68c3-4d0d-b27e-6833785b62fc.containerhub.tripleten-services.com',
 };
